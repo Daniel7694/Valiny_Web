a6d9a415a9b41abfa6967fe44b33a875eb1ad7f2
HeidyPrieto <choripapa2050@gmail.com>
2024-06-04 20:53:27 -0500

    (bug): Background menu

--- src/Components/Cursos.jsx
+++ src/Components/Cursos.jsx
@@ -6,34 +6,21 @@ import { FaFilePdf } from 'react-icons/fa';
 import Menu from './menú';
 import { PDFDownloadLink } from '@react-pdf/renderer';
 import CursosPdf from './CursosPdf';
-import MyPdfViewer from './MyPdfViewer';
 import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
-
 
 const Cursos = () => {
   const [students, setStudents] = useState([]);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [showPdf, setShowPdf] = useState(false);
+  const [selectedCourse, setSelectedCourse] = useState('501'); // Estado para almacenar el curso seleccionado
 
   const fecha = new Date();
-  const fechaFormateada = `${fecha.getDate()}-${fecha.getMonth()+1}-${fecha.getFullYear()}`;
-
-  const [token, setToken] = useState(null);
-
-  useEffect(() => {
-    const storedToken = localStorage.getItem('token');
-
-    if (storedToken) {
-      setToken(storedToken);
-      console.log(token);
-    }
-  }, [token]);
+  const fechaFormateada = `${fecha.getDate()}-${fecha.getMonth() + 1}-${fecha.getFullYear()}`;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://10.175.83.138:3000/api/estudiantes');
+        const response = await axios.get('http://192.168.1.15:3000/api/estudiantes');
         setStudents(response.data.data);
       } catch (error) {
         console.error('Error fetching data: ', error);
@@ -43,48 +30,26 @@ const Cursos = () => {
     fetchData();
   }, []);
 
-  useEffect(() => {
-    return () => {
-    //  setShowPdf(false);
-    };
-  }, []);
-
   const handleMenu = () => {
     setIsMenuOpen(true);
   };
 
-  const closeMenu = () => {
-    setIsMenuOpen(false);
-  //  setShowPdf(false);
-  };
   const exportarAExcel = (datosApi, nombreArchivo) => {
     const tipoArchivo = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
     const extensionArchivo = '.xlsx';
-  
+
     const hoja = XLSX.utils.json_to_sheet(datosApi);
     const libro = { Sheets: { 'data': hoja }, SheetNames: ['data'] };
     const bufferExcel = XLSX.write(libro, { bookType: 'xlsx', type: 'array' });
     const datos = new Blob([bufferExcel], { type: tipoArchivo });
     FileSaver.saveAs(datos, nombreArchivo + extensionArchivo);
   };
-  
 
-  const handleInstructions = () => {
-    setShowPdf(true);
-    setIsMenuOpen(false); // Cierra el menú cuando se abre el PDF
-    console.log('showPdf:', showPdf);
-  };
-
-  console.log('showPdf:', showPdf); // Agregamos un console.log aquí para verificar el valor de showPdf
-
-  if (showPdf) {
-    console.log('showPdf is true'); // Agregamos un console.log aquí para verificar si el bloque condicional se está ejecutando
-    return <MyPdfViewer file="/ManualDeInstruccionesValiny.pdf" />;
-  }
+  const filteredStudents = students.filter(student => student.Curso === selectedCourse); // Filtra estudiantes por el curso seleccionado
 
   return (
     <div className="relative container mx-auto px-4 sm:px-8">
-  {isMenuOpen && <Menu setToken={setToken} onClose={() => setIsMenuOpen(false)} onInstructionsClick={handleInstructions} />}
+      {isMenuOpen && <Menu onClose={() => setIsMenuOpen(false)} />}
       <div className="py-8">
         <div className='flex flex-row'>
           <button onClick={handleMenu}>
@@ -93,6 +58,19 @@ const Cursos = () => {
           <h2 className='text-5xl text-center mb-5 px-96'>Lista de cursos</h2>
         </div>
         <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+          <div className="mb-4">
+            <label htmlFor="curso" className="text-lg font-semibold mb-4">Seleccione un curso:</label>
+            <select
+              id="curso"
+              value={selectedCourse}
+              onChange={(e) => setSelectedCourse(e.target.value)}
+              className="border border-gray-300 rounded px-2 py-1"
+            >
+              <option value="501">Curso 501</option>
+              <option value="502">Curso 502</option>
+              <option value="503">Curso 503</option>
+            </select>
+          </div>
           <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
             <table className="min-w-full leading-normal">
               <thead>
@@ -112,7 +90,7 @@ const Cursos = () => {
                 </tr>
               </thead>
               <tbody>
-                {students.map((student, index) => (
+                {filteredStudents.map((student, index) => (
                   <tr key={student.Documento} className={index % 2 === 0 ? 'bg-white' : 'bg-blue-200'}>
                     <td className="px-5 py-5 border-b border-gray-200 text-sm font-family: ui-serif, Georgia, Cambria, Times New Roman Times, serif">{index + 1}</td>
                     <td className="px-5 py-5 border-b border-gray-200 text-sm font-family: ui-serif, Georgia, Cambria, Times New Roman Times, serif">{student.Documento}</td>
@@ -125,23 +103,21 @@ const Cursos = () => {
           </div>
           <h2>Descargar en:</h2>
           <div className="flex justify-center space-x-10 mt-10">
-  <div className="flex flex-col items-center">
-    <PDFDownloadLink document={<CursosPdf students={students} />} fileName={`Registro_de_lista_de_cursos-${fechaFormateada}.pdf`}>
-      {({ blob, url, loading, error }) =>
-        loading ? 'Cargando documento...' : <FaFilePdf size={50} color="red"/>
-      }
-    </PDFDownloadLink>
-    <span>PDF</span>
-  </div>
-  <div className="flex flex-col items-center">
-    <button onClick={() => exportarAExcel(students, `Lista_de_cursos_${fechaFormateada}`)}>
-      <FaFileExcel size={50} color="green"/>
-    </button>
-    <span>Excel</span>
-  </div>
-</div>
-
-
+            <div className="flex flex-col items-center">
+              <PDFDownloadLink document={<CursosPdf students={filteredStudents} />} fileName={`Registro_de_lista_de_cursos-${fechaFormateada}.pdf`}>
+                {({ blob, url, loading, error }) =>
+                  loading ? 'Cargando documento...' : <FaFilePdf size={50} color="red"/>
+                }
+              </PDFDownloadLink>
+              <span>PDF</span>
+            </div>
+            <div className="flex flex-col items-center">
+              <button onClick={() => exportarAExcel(filteredStudents, `Lista_de_cursos_${fechaFormateada}`)}>
+                <FaFileExcel size={50} color="green"/>
+              </button>
+              <span>Excel</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/Components/menú.jsx
+++ src/Components/menú.jsx
@@ -1,36 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { FaInfoCircle, FaBookReader, FaRegListAlt } from 'react-icons/fa';
-import MyPdfViewer from './MyPdfViewer'; // Importa el componente MyPdfViewer
-import { useContext } from 'react';
-import { UserContext } from '../App'; // Importa el contexto
+import MyPdfViewer from './MyPdfViewer';
+import { UserContext } from '../App';
 
 function Menu({ setToken, onClose, onInstructionsClick }) {
   const [showPdf, setShowPdf] = useState(false);
   const navigate = useNavigate();
-  const [admin, setAdmin] = useState([]);
+  const [admin, setAdmin] = useState(null); // Cambiado a null para inicialización correcta
+  const { userData } = useContext(UserContext);
 
-  const { userData } = useContext(UserContext); // Usa el contexto
+  // Asegúrate de que userData tiene el ID_Admin antes de hacer la petición
+  useEffect(() => {
+    const fetchData = async () => {
+      if (userData && userData.ID_Admin) {
+        try {
+          const response = await axios.get(`http://192.168.1.15:3000/api/administradores/${userData.ID_Admin}`);
+          setAdmin(response.data.data);
+        } catch (error) {
+          console.error('Error fetching data: ', error);
+        }
+      }
+    };
 
-console.log(userData.ID_Admin);
-
-useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(`http://10.175.83.138:3000/api/administradores/${userData.ID_Admin}`);
-      setAdmin(response.data.data);
-
-    } catch (error) {
-      console.error('Error fetching data: ', error);
-    }
-  };
-
-  fetchData();
-}, []);
+    fetchData();
+  }, [userData]);
 
   const handleLogout = () => {
-    localStorage.removeItem('token'); 
+    localStorage.removeItem('token');
     setToken(null);
     window.history.pushState(null, "", window.location.href);
     window.onpopstate = function(event) {
@@ -53,14 +51,15 @@ useEffect(() => {
     setShowPdf(true);
     onClose();
   };
+
   const imgHandler = onInstructionsClick ? onInstructionsClick : (
     <img 
       className='w-full h-full mb-4 mx-auto my-auto opacity-50'  
       src="/colegio.jpg" 
       alt="Imagen por defecto" 
-    />  );
+    />
+  );
 
-  // Si no se proporciona onInstructionsClick, se utiliza handleInstructions
   const instructionsHandler = onInstructionsClick ? onInstructionsClick : handleInstructions;
 
   if (showPdf) {
@@ -71,38 +70,40 @@ useEffect(() => {
     <div className="fixed inset-0 flex z-50 rounded-md">
       <div className="w-96 h-full bg-white rounded-md font-serif shadow-lg relative">
         <button onClick={handleLogout} className="mt-4 text-blue-500 p-4">Log Out</button>
-        <div className="mt-12 p-4 text-center">
-          <p className="font-bold text-lg">{admin.Nombres}</p>
-          <p>{admin.Rol}</p>
-        </div>
+        {admin ? (
+          <div className="mt-12 p-4 text-center">
+            <p className="font-bold text-lg">{admin.Nombres}</p>
+            <p>{admin.Rol}</p>
+          </div>
+        ) : (
+          <div className="mt-12 p-4 text-center">
+            <p>Cargando datos del administrador...</p>
+          </div>
+        )}
         <ul className="mt-8">
-  { admin.Rol === 'SuperAdmin' && (
-    <button onClick={handleReportes} className='w-full'>
-      <li className="mt-12 flex items-center p-4 hover:bg-blue-100">
-        <FaRegListAlt size={20} color="#3B82F6" />
-        <span className="ml-2">Reportes</span>
-      </li>
-    </button>
-  )}
-  <button onClick={handleCursos} className='w-full'>
-    <li className="flex items-center p-4 hover:bg-blue-100">
-      <FaBookReader size={20} color="#3B82F6" />
-      <span className="ml-2">Cursos</span>
-    </li>
-  </button>
-</ul>
-<div className="absolute bottom-0 left-0 ">
-  <button onClick={instructionsHandler} className="flex items-center justify-center p-4 hover:bg-blue-200 w-full">
-    <FaInfoCircle size={20} color="#3B82F6" />
-    <span className="ml-2">Manual de instrucciones</span>
-  </button>
-</div>
-
+          {admin && admin.Rol === 'SuperAdmin' && (
+            <button onClick={handleReportes} className='w-full'>
+              <li className="mt-12 flex items-center p-4 hover:bg-blue-100">
+                <FaRegListAlt size={20} color="#3B82F6" />
+                <span className="ml-2">Reportes</span>
+              </li>
+            </button>
+          )}
+          <button onClick={handleCursos} className='w-full'>
+            <li className="flex items-center p-4 hover:bg-blue-100">
+              <FaBookReader size={20} color="#3B82F6" />
+              <span className="ml-2">Cursos</span>
+            </li>
+          </button>
+        </ul>
+        <div className="absolute bottom-0 left-0 ">
+          <button onClick={instructionsHandler} className="flex items-center justify-center p-4 hover:bg-blue-200 w-full">
+            <FaInfoCircle size={20} color="#3B82F6" />
+            <span className="ml-2">Manual de instrucciones</span>
+          </button>
+        </div>
       </div>
-      <div 
-      
-      className="flex-grow bg-black bg-opacity-50" onClick={onClose}>{imgHandler}</div>
-      
+      <div className="flex-grow bg-black bg-opacity-50" onClick={onClose}>{imgHandler}</div>
     </div>
   );
 }
